cd0000ddafea4c6429db23fef139d7cae4d28a20
Jeff Nichols <jnichols@lyntron.com>
2015-10-30 15:18:49 -0700

    Initial Commit
    Signed-off-by: Jeff Nichols <jnichols@lyntron.com>

---
+++ rbacData/rbacData/rbacDataLayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rbacData
+{
+    public class User
+    {
+        public string UserId { get; set; } //User id
+        public string UserName { get; set; } //User Name
+        public string Password { get; set; } //User Password
+    }
+
+    class rbacDataLayer
+    {
+    }
+}
